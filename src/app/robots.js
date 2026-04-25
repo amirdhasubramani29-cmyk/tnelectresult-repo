63014a68bd531ba51c4da3e7d5b4e165ea30d3f3
Sasikumar Appasamy <sasikumar.appasamy@gmail.com>
2026-04-25 09:22:05 +0530

    Sitemap update

---
+++ src/app/robots.js
@@ -0,0 +1,9 @@
+export default function robots() {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://tnelectionresults.site/sitemap.xml",
+  };
+}
---
+++ src/app/sitemap.js
@@ -0,0 +1,14 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://tnelectionresults.site",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://tnelectionresults.site/tn/2026",
+    },
+    {
+      url: "https://tnelectionresults.site/tn/2021",
+    },
+  ];
+}
