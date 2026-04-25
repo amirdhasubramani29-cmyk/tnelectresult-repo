db4cc3b4603cf8e8be5e83eca108bd328c23891a
Sasikumar Appasamy <sasikumar.appasamy@gmail.com>
2026-04-25 12:50:00 +0530

    Vercel analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,6 +6,7 @@ import MainHeader from "@/components/MainHeader";
 import Script from "next/script";
 import ScrollToTop from "@/components/ScrollToTop";
 import Footer from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"], variable: '--font-inter' });
 const notoTamil = Noto_Sans_Tamil({ subsets: ["tamil"], variable: '--font-tamil', weight: ['400','500','600','700'] });
@@ -44,7 +45,8 @@ export default function RootLayout({ children }) {
 
           {/* ✅ FOOTER */}
           <Footer />
-
+		
+		  <Analytics />
         </AppProvider>
       </body>
     </html>
